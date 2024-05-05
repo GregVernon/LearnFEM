b4efc9808946b22c49f9643c65a5bdaf5d16a0c3
gvernon2 <greg.j.vernon@gmail.com>
2024-05-05 13:01:16 -0600

    updates to companion matrix

--- lectures/lecture_01/CompanionMatrix.m
+++ lectures/lecture_01/CompanionMatrix.m
@@ -1,6 +1,11 @@
 function C = CompanionMatrix( polyfun )
 c = fliplr( coeffs( polyfun, 'all' ) );
-C = sym( zeros( length(c) - 1 ) );
-C(2:end, 1:end-1) = eye( length(c) - 2 );
-C(:, end) = - ( c( 1:end-1 ) ./ c(end) );
+if length( c ) > 2
+    C = sym( zeros( length(c) - 1 ), "r" );
+    C(2:end, 1:end-1) = eye( length(c) - 2 );
+    C(:, end) = - ( c( 1:end-1 ) ./ c(end) );
+elseif length( c ) == 2
+    C =  c(1) / (-c(2)) ;
+elseif isscalar( c )
+    C = [];
 end

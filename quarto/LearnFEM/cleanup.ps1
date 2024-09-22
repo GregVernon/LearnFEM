e2c9fcd4729ffe18d4374fca1609d51c6a496b6b
gvernon2 <greg.j.vernon@gmail.com>
2024-09-22 00:22:12 -0600

    add a cleanup script

---
+++ quarto/LearnFEM/cleanup.ps1
@@ -0,0 +1,13 @@
+rm .\lecture_00\change_of_basis_files -r -Force
+rm .\lecture_00\change_of_variable_files -r -Force
+rm .\lecture_00\inner_products_files -r -Force
+rm .\lecture_00\polynomial_basis_files -r -Force
+rm .\lecture_00\polynomial_roots_files -r -Force
+
+rm .\lecture_01\galerkin_function_approximation_files -r -Force
+rm .\lecture_01\galerkin_function_approximation_with_boundary_conditions_files -r -Force
+
+rm .\lecture_02\galerkin_poisson_equation_files -r -Force
+
+rm .\index_files -r -Force
+rm .\_book -r -Force
